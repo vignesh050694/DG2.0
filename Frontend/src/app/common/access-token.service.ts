939b9fa06c674e413ab13be2a8efbd59c907bf6e
vvignesh <vvignesh@calpyte.com>
2023-06-15 13:38:01 +0530

    Added realm in header

--- Frontend/src/app/common/access-token.service.ts
+++ Frontend/src/app/common/access-token.service.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,8 @@ import { Observable } from 'rxjs';
 export class AccessTokenService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const token = localStorage.getItem("generatedToken");
-
-    if (!token) {
-      return next.handle(req);
-    }
-
     const req1 = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
+      headers: req.headers.set('realm', environment.realm),
     });
 
     return next.handle(req1);
